1d01872f40b8efe89ea33e45f7dd7ffafa322431
Orses <orseton@gmail.com>
2022-07-20 22:24:45 +0200

    deleted all linear search algorithm

--- src/ts/searching/searchLinearTable.test.ts
+++
@@ -1,9 +0,0 @@
-import searchLinearTable from './searchLinearTable';
-
-describe('Basic test', () => {
-  test('Should work for small and basic array', () => {
-    const expected = searchLinearTable([90, 70, 50, 80, 60, 85], 60);
-    const result = 4;
-    expect(expected).toBe(result);
-  });
-});
