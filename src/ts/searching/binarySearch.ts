02fbab9cc7d841b31177923f1454264835773f95
Orses <orseton@gmail.com>
2022-07-22 22:11:48 +0200

    New algorithm

--- src/ts/searching/binarySearch.ts
+++ src/ts/searching/binarySearch.ts
@@ -1,28 +1,22 @@
 /**
- * This function make a binary search on an array
- *
+ * This function search a value in an ordered array data
  *
  ** Algorithm: Binary Search Sort
  ** Complexity: O(log n)
- * @param {number:[]} data
+ * @param {number[]} data - The ordered data
  * @param {number} key - The value to find in data
  * @returns {number} The index of the found value or -1 if key doesn't exists
  */
 function binarySearch(data: number[], key: number): number {
   let start = 0;
   let end = data.length - 1;
-
-  function getMiddle(low: number, hight: number): number {
-    return Math.trunc((low + hight) / 2);
-  }
-
-  let middle = getMiddle(start, end);
+  let middle = 0;
 
   while (start <= end) {
+    middle = Math.trunc((start + end) / 2);
     if (data[middle] === key) return middle;
     if (data[middle] < key) start = middle + 1;
     if (data[middle] > key) end = middle - 1;
-    middle = getMiddle(start, end);
   }
 
   return -1;
